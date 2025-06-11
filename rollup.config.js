bc0ff62429420de91d16110e85c45dc4c911f35b
kshetline <kerry@shetline.com>
2025-06-11 17:13:03 -0400

    Update build config.

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,10 @@ const terser = require('@rollup/plugin-terser');
 const typescript = require('@rollup/plugin-typescript');
 const pkg = require('./package.json');
 
-module.exports = [
-  {
-    input: 'src/index.ts',
-    output: [{
+module.exports = [{
+  input: 'src/index.ts',
+  output: [
+    {
       file: pkg['umd:main'],
       sourcemap: true,
       format: 'umd',
